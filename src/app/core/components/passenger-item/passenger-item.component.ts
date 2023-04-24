119332a952e5af462851f02b0439adcc7a28d0d0
Maryia Piarshai <mary.pershay@gmail.com>
2023-04-24 18:07:04 +0300

    feat: booking form html

---
+++ src/app/core/components/passenger-item/passenger-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-item',
+  templateUrl: './passenger-item.component.html',
+  styleUrls: ['./passenger-item.component.scss'],
+})
+export class PassengerItemComponent {
+  @Input()
+  public passenger!: string;
+}
