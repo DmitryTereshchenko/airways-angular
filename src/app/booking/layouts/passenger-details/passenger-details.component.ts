6c3344636cfbbb136604a18451f510ac9e5e39ad
Maryia Piarshai <mary.pershay@gmail.com>
2023-04-26 14:21:46 +0300

    feat: details page form validation

--- src/app/booking/layouts/passenger-details/passenger-details.component.ts
+++ src/app/booking/layouts/passenger-details/passenger-details.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-passenger-details',
@@ -10,13 +15,28 @@ export class PassengerDetailsComponent implements OnInit {
   public bookingForm!: FormGroup;
   public passengerList = ['adult', 'child', 'child', 'infant'];
 
+  constructor(private fb: FormBuilder) {}
+
   public ngOnInit(): void {
-    this.bookingForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      // desc: new FormControl('', [Validators.maxLength(255)]),
-      // img: new FormControl('', [Validators.required, Validators.pattern(urlPattern)]),
-      // linkVideo: new FormControl('', [Validators.required, Validators.pattern(urlPattern)]),
-      // creationDate: new FormControl('', [Validators.required, DateValidators.checkDate]),
+    this.bookingForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      countryCode: ['', [Validators.required]],
+      passengers: this.fb.array([]),
     });
+  }
+
+  public onSubmit(): void {
+    const { controls } = this.bookingForm;
+
+    if (this.bookingForm.invalid) {
+      Object.keys(controls).forEach((controlName) =>
+        controls[controlName].markAsTouched()
+      );
+
+      return;
+    }
+
+    console.log(this.bookingForm.value);
   }
 }
