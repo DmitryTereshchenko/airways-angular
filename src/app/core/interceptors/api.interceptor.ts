f346af71991d7b02b27d5aa99b018fde1523f127
Dzmitry Tsiareshchanka <dima.tereshchenko.96@mail.ru>
2023-05-28 23:34:49 +0400

    fix: http

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   private readonly apiURL =
-    'https://ec2-52-31-76-72.eu-west-1.compute.amazonaws.com';
+    'https://ec2-52-31-76-72.eu-west-1.compute.amazonaws.com/';
 
   public intercept(
     request: HttpRequest<unknown>,
